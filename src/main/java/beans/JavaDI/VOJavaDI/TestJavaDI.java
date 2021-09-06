383f64799cb4a065284a4642501b3918906af5cf
kdd9512 <hyunwoo8805@gmail.com>
2021-09-07 01:40:06 +0900

    20210906 commit
    spring 재복습중...

---
+++ src/main/java/beans/JavaDI/VOJavaDI/TestJavaDI.java
@@ -0,0 +1,49 @@
+package beans.JavaDI.VOJavaDI;
+
+import beans.JavaDI.DataJavaDI;
+
+public class TestJavaDI {
+    private int data1;
+    private String data2;
+    private DataJavaDI data3;
+
+    public TestJavaDI() {
+
+    }
+
+    public TestJavaDI(int data1, String data2, DataJavaDI data3) {
+        this.data1 = data1;
+        this.data2 = data2;
+        this.data3 = data3;
+    }
+
+    public int getData1() {
+        return data1;
+    }
+
+    public void setData1(int data1) {
+        this.data1 = data1;
+    }
+
+    public String getData2() {
+        return data2;
+    }
+
+    public void setData2(String data2) {
+        this.data2 = data2;
+    }
+
+    public DataJavaDI getData3() {
+        return data3;
+    }
+
+    public void setData3(DataJavaDI data3) {
+        this.data3 = data3;
+    }
+
+    public void printData() {
+        System.out.printf("t18 : %d\n",data1);
+        System.out.printf("t18 : %s\n",data2);
+        System.out.printf("t18 : %s\n",data3);
+    }
+}
---
+++ src/main/java/beans/JavaDI/VOJavaDI/TestJavaDI2.java
@@ -0,0 +1,28 @@
+package beans.JavaDI.VOJavaDI;
+
+import beans.JavaDI.DataJavaDI2;
+
+public class TestJavaDI2 {
+    private DataJavaDI2 data1;
+    private DataJavaDI2 data2;
+
+    public TestJavaDI2() {
+    }
+
+    public DataJavaDI2 getData1() {
+        return data1;
+    }
+
+    public void setData1(DataJavaDI2 data1) {
+        this.data1 = data1;
+    }
+
+    public DataJavaDI2 getData2() {
+        return data2;
+    }
+
+    public void setData2(DataJavaDI2 data2) {
+        this.data2 = data2;
+    }
+
+}
