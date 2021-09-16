d54b5dea21a58fca336310435a2dcbe672e04677
kdd9512 <hyunwoo8805@gmail.com>
2021-09-16 23:13:08 +0900

    20210916 commit
    spring 재복습중...

--- src/main/java/beans/ExecutionDirective/ExecutionBeans/TestBeanExe.java
+++ src/main/java/beans/ExecutionDirective/ExecutionBeans/TestBeanExe.java
@@ -10,4 +10,19 @@ public class TestBeanExe {
         System.out.printf("BeanExc(int a1) : %d\n",a1);
     }
 
+    public void BeanExc (String s1) {
+        System.out.printf("BeanExc(java.lang.String s1) : %s\n",s1);
+    }
+
+    public void BeanExc (int a1, int a2) {
+        System.out.printf("BeanExc(int a1, int a2) : %d, %d\n",a1,a2);
+    }
+
+    public void BeanExc (int a1, String s1) {
+        System.out.printf("BeanExc(int a1, String s1) : %d, %s\n",a1,s1);
+    }
+
+    public void BeanExc2 (){
+        System.out.println("BeanExc2 메서드 호출됨");
+    }
 }
--- src/main/java/main/MainExecutionDirective.java
+++ src/main/java/main/MainExecutionDirective.java
@@ -14,9 +14,29 @@ public class MainExecutionDirective {
         t34.BeanExc();
 
         // 관심사로 등록되어있지 않으면 weaving(Advice 를 핵심 로직에 적용) 되지 않는다.
-        // 즉 AdvisorClass 내의 메서드가 같이 동작을 안함.
+        // 즉 AdvisorClass 내의 메서드(beforeEx 메서드)가 같이 동작을 안함.
         System.out.println("============== 관심사로 등록되지 않은 메서드 ===============");
+
+        // TestBeanExe.BeanExc 중, int 를 매개변수로 갖는 메서드 호출
         t34.BeanExc(1972);
+
+        System.out.println("-------------------------------------------");
+
+        // TestBeanExe.BeanExc 중, String 을 매개변수로 갖는 메서드 호출
+        t34.BeanExc("심영");
+
+        System.out.println("-------------------------------------------");
+
+        // TestBeanExe.BeanExc 중, 2개의 int 타입 매개변수를 갖는 메서드 호출.
+        t34.BeanExc(1972, 1121);
+
+        System.out.println("-------------------------------------------");
+        // TestBeanExe.BeanExc 중, int 타입, String 타입의 매개변수를 각각 1개씩 갖는 메서드 호출.
+        t34.BeanExc(1972, "김두한");
+
+        System.out.println("-------------------------------------------");
+        // TestBeanExe.BeanExc2 호출
+        t34.BeanExc2();
 
         ctx.close();
     }
