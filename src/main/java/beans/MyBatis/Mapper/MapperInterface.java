99ac8dc3b01299a7e0013e3b92a0c21fa38ee079
kdd9512 <hyunwoo8805@gmail.com>
2021-09-21 23:30:43 +0900

    20210921 commit
    spring 재복습중...

--- src/main/java/beans/MyBatis/Mapper/MapperInterface.java
+++ src/main/java/beans/MyBatis/Mapper/MapperInterface.java
@@ -1,5 +1,28 @@
 package beans.MyBatis.Mapper;
 
+import beans.MyBatis.MbBeans.JDBCBeanMB;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
 public interface MapperInterface {
 
+//    @Results({
+//            @Result(column = "intData", property = "intData"),
+//            @Result(column = "strData", property = "strData")
+//    })
+    @Select("select intData, strData from jdbcTest")
+    List<JDBCBeanMB> selectData();
+
+    @Insert("insert into jdbcTest (intData, strData) values (#{intData}, #{strData})")
+    // insert 에 반환값이 존재할 리 없으므로(값을 입력하는 용도지 받아오는 용도가 아니다) void 처리.
+    void insertData(JDBCBeanMB beanMB);
+
+    @Update("update jdbcTest set intData = #{intData} where strData = #{strData}")
+    // update 에 반환값이 존재할 리 없으므로(값을 입력하는 용도지 받아오는 용도가 아니다) void 처리.
+    void updateData(JDBCBeanMB beanMB);
+
+    @Delete("delete from jdbcTest where strData = #{strData}")
+    // update 에 반환값이 존재할 리 없으므로(값을 입력하는 용도지 받아오는 용도가 아니다) void 처리.
+    void deleteData(String strData);
 }
--- src/main/java/main/MainMyBatis.java
+++ src/main/java/main/MainMyBatis.java
@@ -2,7 +2,10 @@ package main;
 
 import beans.MyBatis.BeanConfigMB;
 import beans.MyBatis.Mapper.MapperInterface;
+import beans.MyBatis.MbBeans.JDBCBeanMB;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
 
 // ** MyBatis 는 반드시 JRE System Library 1.8 이상의 JAVA 에서 설치되어야 한다.(이하 버전에서는 컴파일 에러가 발생함.)
 // 확인 후 MyBatis 와 MyBatis Spring 을 설치해야 한다.(Maven Repository 에서 검색)
@@ -12,6 +15,34 @@ public class MainMyBatis {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(BeanConfigMB.class);
 
         MapperInterface mapper = ctx.getBean("getMapper",MapperInterface.class);
+
+        // insert
+//        JDBCBeanMB bean2 = new JDBCBeanMB();
+//        bean2.setIntData(1972);
+//        bean2.setStrData("으사양반");
+//        mapper.insertData(bean2);
+//
+//        JDBCBeanMB bean3 = new JDBCBeanMB();
+//        bean3.setIntData(1972);
+//        bean3.setStrData("상하이조");
+//        mapper.insertData(bean3);
+
+        // update
+//        JDBCBeanMB bean4 = new JDBCBeanMB();
+//        bean4.setStrData("상하이조");
+//        bean4.setIntData(9999);
+//        mapper.updateData(bean4);
+
+        // delete
+//        mapper.deleteData("상하이조");
+
+        // select
+        List<JDBCBeanMB> list1 = mapper.selectData();
+        for (JDBCBeanMB bean1 : list1) {
+            System.out.printf("intData : %d\n",bean1.getIntData());
+            System.out.printf("strData : %s\n",bean1.getStrData());
+            System.out.println("=========================================");
+        }
 
         ctx.close();
     }
